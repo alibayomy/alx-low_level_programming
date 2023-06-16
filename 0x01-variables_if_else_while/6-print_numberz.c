66ad10e05ede8058e2631547a6d0c7d0d3c02ed8
alibayomy <alibayomy12@gmail.com>
2023-06-16 09:56:30 -0700

    excuting tasks

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Description: print all 1 digit nums with putchar
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48 ; n < 58; n++)
+	{
+		putchar ("%d", n);
+	}
+	putchar (10);
+	return (0);
+}
