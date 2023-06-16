a55c812abb9fb96d61040de835d80ad3aee1cdcb
alibayomy <alibayomy12@gmail.com>
2023-06-15 18:02:20 -0700

    first code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
-#!/bin/bash
 #include <stdio.h>
+/**
+ * main - entery point
+ *
+ * Description : a  c program that prints with put function
+ * Retrun always (0) success
+ **/
 int main (void) {
-	puts("Programming is like building a multilingual puzzle");
-		return 0;
-
+	puts ("\"Programming is like building a multilingual puzzle");
+		return (0);
 };
