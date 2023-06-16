35a465633b087a5b17adf5d1a7cae98766304ad7
alibayomy <alibayomy12@gmail.com>
2023-06-15 18:39:04 -0700

    aaa

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main : Entery point
+ *
+ * Description :A c program prints the size of various types on the computer.
+ *
+ * Return : Always 0 (success)
+ */
+int main(void)
+{
+	printf("Size of a char : %lu.\n", sizeof(char));
+	printf("Size of an int : %lu.\n", sizeof(int));
+	printf("Size of a long int : %lu.\n", sizeof(long int));
+	printf("Size of a long long int : %lu.\n", sizeof(long long int));
+	printf("Size of a float : %lu.\n", sizeof(float));
+}
