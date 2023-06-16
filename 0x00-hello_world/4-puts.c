770bc3dad4efce2595adc22cdcde3cb7419acb8a
alibayomy <alibayomy12@gmail.com>
2023-06-15 18:15:41 -0700

    debugging

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,8 @@
  * Description : a  c program that prints with put function
  * Retrun always (0) success
  **/
-int main (void) {
-	puts ("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
 		return (0);
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main (void) {
-	printf ("with proper grammar, but the outcome is a piece of art,\n");
+/**
+ * main - Entery point
+ * Description : c program that uses printf funtion
+ * Return: always (0) success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
 }
