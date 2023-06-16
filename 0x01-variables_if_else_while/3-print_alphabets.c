2cda8c2ef2d8e0fd024ddbc380d0afc689cbcace
alibayomy <alibayomy12@gmail.com>
2023-06-16 09:16:43 -0700

    excuting tasks

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Description: print alphabet in lowercase and uppercase
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 97; n < 123; n++)
+	{
+		putchar(n);
+	}
+	for (n = 60; n < 91; n++)
+	{
+		putchar(n);
+	}
+	putchar(10);
+	return (0);
