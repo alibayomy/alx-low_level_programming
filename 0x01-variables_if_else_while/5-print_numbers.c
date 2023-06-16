1db840bd578d46a794280fdd6e9c74e8504ba12f
alibayomy <alibayomy12@gmail.com>
2023-06-16 09:45:56 -0700

    excuting tasks

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Entery point
+ *
+ * Description: print all numbers in 1 digit
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	return (0)
+}
