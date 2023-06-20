1d3b7be9be3f33025c2b8b7544899c67d9551762
alibayomy <alibayomy12@gmail.com>
2023-06-20 15:51:38 -0700

    file num 2

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ * main - Entery point
+ *
+ * print_alphabet: helper function that prints the lowercase
+ *
+ * Discription: print all the alphabet with new line
+ *
+ * Return: always 0 (success)
+ */
+void print_alphabet(void)
+{
+	for (i = 97; i < 123; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ('\n');
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
