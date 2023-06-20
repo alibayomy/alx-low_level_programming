0a83f7d3669f8b89b7038ce9f13dca5dd2c598f4
alibayomy <alibayomy12@gmail.com>
2023-06-20 16:51:45 -0700

    excuting file num 3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,6 +4,10 @@
 /**
  * _islower - take a char and return 0 if it is a lower char
  *		and 1 if it is an upper char
+ *
+ * @c: the function parameter
+ *
+ * Return: 1 if lower case and 0 if an upper case
  */
 int _islower(int c)
 {
