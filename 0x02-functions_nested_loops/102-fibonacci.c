9dbcb6d86c7ba41ac436b2d7c1bb37d093674de0
alibayomy <alibayomy12@gmail.com>
2023-06-21 16:55:05 -0700

    excuting advanced file num 102

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entery point of the program
+ *
+ * Discription: print the first 50 numbers of fibonacci numbers
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int f = 1, s = 2, sum, i;
+	
+	for (i = 1 ; i < 51; i++)
+	{
+		sum = x + y;
+		printf ("%d, ", sum);
+		y = sum;
+		x = y;
+	}
+	return (0);
+}
