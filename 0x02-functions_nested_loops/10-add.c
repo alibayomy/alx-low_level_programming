e8aab9627b291a912976c855a15944f86693d6cc
alibayomy <alibayomy12@gmail.com>
2023-06-21 14:17:21 -0700

    excuting file num 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * add - adds two integers
+ *
+ * @x: first argument of the func in int type
+ *
+ * @y: second argument of int type
+ *
+ * Return: the sum of two integers
+ */
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
