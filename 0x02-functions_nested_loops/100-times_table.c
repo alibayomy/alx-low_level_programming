34bb001c411ca6e6a459521a7bd868a35d1387c1
alibayomy <alibayomy12@gmail.com>
2023-06-21 15:11:15 -0700

    excuting advanced file num 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,17 +23,22 @@ void print_times_table(int n)
 			{
 				_putchar (',');
 				_putchar (' ');
-				if ((y + i) > 9)
+				if ((y + i) < 99 && ((y + i) > 9))
 				{
+					_putchar(' ');
 				_putchar (((y + i) / 10) + 48);
 				_putchar (((y + i) % 10) + 48);
 				}
-				else
+				else if ((y + i) < 9)
 				{
 					_putchar (' ');
 					_putchar (' ');
 					_putchar (((y + i) % 10) + 48);
 				}
+				else
+				{
+					_putchar (((y + i) / 100) + 48);
+					_putchar ((((y + i) / 10) % 10) + 48);
 				y = y + i;
 			}
 			_putchar ('\n');
