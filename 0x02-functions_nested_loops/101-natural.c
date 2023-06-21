d72777ca64bc8613809bf3f88cdcae72166aa32d
alibayomy <alibayomy12@gmail.com>
2023-06-21 16:16:35 -0700

    excuting advanced file num 101

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * natural - prints the sum of the numbers that are multiples
- *      of 3 and 5 and lower than 1024
+ * main - Entery point of the program
+ *
+ * Discription: print all the numbers that multiple
+ *				of 3 and 5 and lower than 1024
+ *
+ * Return: always 0 means success
  */
-void natural(void)
+void main(void)
 {
-        int i = 0;
+	int i = 0;
 
-        int sum = 0;
+	int sum = 0;
 
-        while (i < 1024)
-        {
-                if (((i % 3) == 0) || (i % 5) == 0)
-                {
-                        sum = sum + i;
-                        i++;
-                }
-        }
-        printf("%d", sum);
+	while (i < 1024)
+	{
+		if (((i % 3) == 0) || (i % 5) == 0)
+		{
+			sum = sum + i;
+			i++;
+		}
+	}
+	printf("%d", sum);
+	return (0);
 }
