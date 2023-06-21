f2a8bd3de7c46a584b51c6d2a88362f7a8a0bd61
alibayomy <alibayomy12@gmail.com>
2023-06-20 17:33:47 -0700

    excuting file num 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,8 +5,10 @@
  * _abs - computes the absolute value of an int
  *
  * @n: function paramter must be int
+ *
+ * Return: the absolute value of an int
  */
 int _abs(int n)
 {
-	abs (n);
+	return (abs(n));
 }
