1c7c0f451a82cee8851886e71605288b0f80318a
alibayomy <alibayomy12@gmail.com>
2023-06-21 14:26:10 -0700

    excuting file num 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_to_98 - print from a given number to num 98
+ *
+ * @n: a given number to start the looping from it
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i < 99; i++)
+	{
+		if (i > 9)
+		{
+			_putchar ((i / 10) + 48);
+			_putchar ((i % 10) + 48);
+		}
+		else
+		{
+			_putchar (' ');
+			_putchar ((i % 10) + 48);
+		}
+		_putchar (',');
+		_putchar (' ');
+	}
+}
