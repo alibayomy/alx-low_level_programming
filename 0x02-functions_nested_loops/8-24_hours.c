789c50436b08af32a891d4cac499fd2e20134a5b
alibayomy <alibayomy12@gmail.com>
2023-06-20 18:22:23 -0700

    excuting file num 7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,7 @@ void jack_bauer(void)
 
 	for (hours = 0; hours < 23; hours++)
 	{
-		for (mins = 0; mins < 60; min++)
+		for (mins = 0; mins < 60; mins++)
 		{
 			_putchar ((hours / 10) + 48);
 			_putchar ((hours % 10) + 48);
