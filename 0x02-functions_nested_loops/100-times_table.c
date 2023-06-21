61eb91a65dfc24b91568ee40029c4c340beb4360
alibayomy <alibayomy12@gmail.com>
2023-06-21 15:02:32 -0700

    excuting advanced file num 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - print a time table as a given number
+ *
+ * @n: the number of times to print time table
+ */
+void print_times_table(int n)
+{
+	int i, num, y;
+
+	if (n > 15 || n < 0)
+	{
+		putchar('$');
+	}
+	else
+	{
+		for (i = 0; i < (n + 1); i++)
+		{
+			y = 0;
+			_putchar (y + 48);
+			for (num = 0; num < n; num++)
+			{
+				_putchar (',');
+				_putchar (' ');
+				if ((y + i) > 9)
+				{
+				_putchar (((y + i) / 10) + 48);
+				_putchar (((y + i) % 10) + 48);
+				}
+				else
+				{
+					_putchar (' ');
+					_putchar (((y + i) % 10) + 48);
+				}
+				y = y + i;
+			}
+			_putchar ('\n');
+		}
+	}
+
+}
