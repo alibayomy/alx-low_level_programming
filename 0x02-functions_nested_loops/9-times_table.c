6caa2b5ebe6180204e512a9b6be737014354fa67
alibayomy <alibayomy12@gmail.com>
2023-06-21 14:07:45 -0700

    excuting file num 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * times_table - print a time all with putchar
+ **/
+void times_table(void)
+{
+	int i, n, y;
+
+	for (i = 0; i < 10; i++)
+	{
+		y = 0;
+	    _putchar (y + 48);
+		for (n = 0; n < 9; n++)
+		{
+			_putchar (',');
+			_putchar (' ');
+			if ((y + i) > 9)
+			{
+				_putchar (((y + i) / 10) + 48);
+				_putchar (((y + i) % 10) + 48);
+			}
+			else
+			{
+				_putchar (((y + i) % 10) + 48);
+			}
+			y = y + i;
+		}
+		_putchar ('$');
+		_putchar ('\n');
+	}
+}
