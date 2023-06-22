55f50b81e03022fa3ea1c8da7667f833b9c1e071
alibayomy <alibayomy12@gmail.com>
2023-06-22 04:18:01 -0700

    excuting file num 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - get the value of multiply 2 integers
+ *
+ * @a: the first number
+ *
+ * @b: the second number
+ *
+ * Return: the sum of multiple of two integers
+ */
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}
