c0a9ef0223b8c9f6451fcc1ab7b3a6980e22db54
alibayomy <alibayomy12@gmail.com>
2023-06-22 05:43:26 -0700

    excuting file num 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line using (\)
+ *
+ * @n: numbers of \ to draw a line
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar ('\n');
+	}
+	else
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar ('\');
+		}
+		_putchar ('\n')
+	}
+}
