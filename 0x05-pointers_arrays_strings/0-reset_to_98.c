06bceabe4862ab453504cf8fe16f0daa64a4bdff
alibayomy <alibayomy12@gmail.com>
2023-06-26 09:35:30 -0700

    excuting  file num 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
- * rest_to_98:  assign a pointer to an int and set its value to 0
+ * reset_to_98  - takes a pointer to an int and
+ *	assign a pointer to an int and set its value to 0
  *
  *@n: function paramter of type pointer
+ *
+ * Reutrn: Nothing
  */
 void reset_to_98(int *n)
 {
