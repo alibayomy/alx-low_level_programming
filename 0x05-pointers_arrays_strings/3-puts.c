36c22a13a31171d390dfaf845d25511e5d33bb3d
alibayomy <alibayomy12@gmail.com>
2023-06-26 10:05:19 -0700

    excuting  file num 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - print a string follwed by new line
+ *
+ * @str: the string that will be printed
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
