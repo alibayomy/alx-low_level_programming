3ad050f12251068df7211b935f3511a2d66cf7e5
alibayomy <alibayomy12@gmail.com>
2023-06-26 13:25:12 -0700

    excuting  file num 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - prints a string in reverse order
+ *
+ * @s: the string that should be printed
+ */
+void print_rev(char *s)
+{
+	printf(strrev(s));
+}
