56eb44ada9b01fe15f48000cf2e09e39809bec02
alibayomy <alibayomy12@gmail.com>
2023-06-27 18:31:49 -0700

    excuting file num 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+/**
+ * _atoi - return the existing numbers into
+ *	a given string
+ *
+ * @s: the string of the function
+ *
+ * Return: the numbers in the string
+ */
+int _atoi(char *s)
+{
+	int n;
+
+	n = atoi(s);
+	return (s);
+}
