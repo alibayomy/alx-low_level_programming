31ce93fabed66d3424b3a4c2103a18bb59e26ada
alibayomy <alibayomy12@gmail.com>
2023-06-27 18:05:39 -0700

    excuting file num 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *_strcpy - copy a string
+ *
+ * @dest: the destination to be copied
+ *
+ * @src: the source to be copied from
+ *
+ * Return: the pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	strcpy(dest, src);
+	return (dest);
+}
