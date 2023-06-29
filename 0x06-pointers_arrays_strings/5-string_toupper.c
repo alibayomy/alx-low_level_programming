4cea1cf838a276c8e80a961d1a03b19a6f3cf5f6
alibayomy <alibayomy12@gmail.com>
2023-06-29 16:53:19 -0700

    excuting file num 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * *string_toupper - convert all the lower case
+ *	letters to an upper case
+ *
+ * @c: the string passed
+ *
+ * Return: the char in uppercase
+ */
+char *string_toupper(char *c)
+{
+	int i;
+
+	for (i = 0; c[i] != '\0'; i++)
+	{
+		if (c[i] > 97)
+		{
+			c[i] = c[i] + 32;
+		}
+	}
+	return (c)
+}
