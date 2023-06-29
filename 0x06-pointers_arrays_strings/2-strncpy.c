38d0101566e5ee70157d53cff3e3b17edd7c758f
alibayomy <alibayomy12@gmail.com>
2023-06-29 13:51:50 -0700

    excuting file num 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * *_strncpy - copies an n chars from src string to
+ *	dest srting.
+ *
+ * @dest: the string to be copied to
+ *
+ * @src: the string to be copied from
+ *
+ * @n: nums of chars to be copied from src
+ *
+ * Return: a pointer to dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i <= n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
