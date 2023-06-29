bcfad6f4e496ac91058b9a6c52504d81e27b3902
alibayomy <alibayomy12@gmail.com>
2023-06-29 14:35:19 -0700

    excuting file num 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,13 +15,9 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
 		dest[i] = src[i];
-	}
-	for (;i > n; i++)
-	{
-		dest[i] = '\0';
 	}
 	return (dest);
 }
