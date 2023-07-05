ed9ddef8138a2d70a6acdf53d42100841c3d8193
alibayomy <alibayomy12@gmail.com>
2023-07-05 13:48:32 -0700

    excuting file  num 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,4 +7,34 @@
  */
 int is_prime_number(int n)
 {
+	prime_helper(n, 2);
+}
+/**
+ * prime_helper - helper function to check if n is prime
+ * @n: the number to check
+ * @i: start from 2 and increasing
+ * Return: 0 if is not prime else 1
+ */
+int prime_helper(int n, int i)
+{
+	if (n == 1 || n <= 0)
+	{
+		return (0);
+	}
+	if (i == n)
+	{
+		return (1);
+	}
 
+	else
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (prime_helper(n, i + 1));
+		}
+	}
+}
