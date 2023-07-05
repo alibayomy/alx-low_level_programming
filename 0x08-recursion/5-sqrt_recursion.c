b624c79d3f50792e1ac3d99e304f89574313e214
alibayomy <alibayomy12@gmail.com>
2023-07-05 13:14:37 -0700

    excuting file  num 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include "4-pow_recursion.c"
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: function paramter , root number to get the natural sqaure of
+ * Return: the natural sqaure of n, (-1) if there is no
+ *	natural sqaure of n
+ */
+int _sqrt_recursion(int n)
+{
+	get_square(n, 1);
+}
+/**
+ * get_square - helper fun get the sqaure of n
+ *@n: thebase number
+ *@i: the root of the number
+ *Return: the natural sqaure of n, (-1) if there is no
+ *	natural sqaure of n
+ */
+int get_square(int n, int i)
+{
+	if (i * i == n)
+	{
+		return (i);
+	}
+	if (i * i > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		get_square(n, i + 1);
+	}
+}
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * is_prime_number - tell if a number is prime or not
+ * @n: the given number
+ * Return: returns 1 if the input integer is a prime number,
+ *	otherwise return 0.
+ */
+int is_prime_number(int n)
+{
+
