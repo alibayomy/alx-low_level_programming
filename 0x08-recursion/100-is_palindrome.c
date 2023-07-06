27104bcf864f7f74cbdd822205a428b17d91042a
alibayomy <alibayomy12@gmail.com>
2023-07-05 18:33:03 -0700

    excuting file 100

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "2-strlen_recursion.c"
+/**
+ * is_palindrome - check if the string is palindrome
+ * @s: the given string
+ * Return: 1 if true 0 if false
+ */
+int is_palindrome(char *s)
+{
+	int n = _strlen_recursion(s);
+
+	return (is_palindrome_helper(s, n, 1));
+/**
+ * is_palindrome_helper - helper function to check
+ *	each letter to its oppsite
+ *@s: the string
+ *@n: size of s
+ *@i: int to reduce from the size to reach the character
+ *Return: 1 if palindrome else 0
+ */
+int is_palindrome_helper(char *s, int n, int i)
+{
+	if (i >= n)
+		return (1);
+	if (s[0] != s[n - i])
+		return (0);
+	if (s[0] == s[n - i] && is_palindrome_helper(s + 1, n, i + 2))
+		return (1);
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,3 +8,4 @@ int _sqrt_recursion(int n);
 int is_prime_number(int n);
 int get_square(int n, int i);
 int prime_helper(int n, int i);
+int is_palindrome(char *s);
