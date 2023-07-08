8e4f30152b3c51754e648d0cbe7a9d7dba68e8d4
alibayomy <alibayomy12@gmail.com>
2023-07-07 18:42:42 -0700

    add file num 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * main - entery point, that adds postive numbers.
+ * @argc: counts the arguments passed  to the program
+ * @argv: an array of the arguments passed
+ * Return:  (0) if success else 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	int sum = 0;
+
+	if (argc < 2)
+	{
+		printf("%d\n", 0);
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		if (atoi (argv[i]) == 0)
+		{
+			printf("Error\n");
+			return (1);
+		}
+		sum +=  atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
