91dcf6fdf0594ac53544a1659d0c4ba365ba982b
alibayomy <alibayomy12@gmail.com>
2023-07-07 17:57:16 -0700

    add file num 2

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entery point , prints how many
+ *	argumets passed into it
+ *@argc: calculate the argument passed
+ *@argv: an array of the argument passed
+ * Return: always (0)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
