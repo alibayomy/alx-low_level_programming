620cc7dd2edd21605a6a6f29a2a1956fffdd4c0e
alibayomy <alibayomy12@gmail.com>
2023-07-10 01:59:36 -0700

    pushing  file num 1

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * create_array - creates an array of chars,
+ *	and initializes it with a specific char.
+ *@size: the size of the array
+ *@c: the type of the array
+ * Return: a pointer to the array , and NULL If size 0
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *str;
+
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+	str = malloc(size);
+	for (i = 0; i <= size; i++)
+	{
+		str[i]  = c;
+	}
+	return (str);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,4 @@
+#include <stddef.h>
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+void *malloc(size_t size);
