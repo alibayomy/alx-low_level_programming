65bb5392c76a4467b1cbb5b85ec6344644c4727e
alibayomy <alibayomy12@gmail.com>
2023-07-10 03:48:11 -0700

    testing file num 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _strdup - reserve a new space in memory, this space 
+ * _strdup - reserve a new space in memory, this space
  *	contains a copy of a string
  *@str: the given string parameter
- * Return: a pointer to a copy of string , or 
+ * Return: a pointer to a copy of string , or
  *	NULL if str = NULL
  */
 char *_strdup(char *str)
@@ -16,9 +16,6 @@ char *_strdup(char *str)
 	str_cpy = malloc(sizeof(*str) * sizeof(str));
 	if (str_cpy == NULL || str == NULL)
 		return (NULL);
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		str_cpy[i] = str[i];
-	}
-	return(str_cpy);
+	str_cpy = str;
+	return (str_cpy);
 }
