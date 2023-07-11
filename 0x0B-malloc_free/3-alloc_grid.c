a93383b4877308d2306ccedd02c90a26efaf34c2
alibayomy <alibayomy12@gmail.com>
2023-07-11 07:57:48 -0700

    debugging file num 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,11 @@ int **alloc_grid(int width, int height)
 		array[i] = (int *) malloc(sizeof(int) * width);
 		if (array[i] == NULL)
 		{
-			free(array[i]);
+			for (i = 0; i < height; i++)
+			{
+				free(array[i]);
+			}
+			free(array);
 			return (NULL);
 		}
 		for (y = 0; y < width ; y++)
