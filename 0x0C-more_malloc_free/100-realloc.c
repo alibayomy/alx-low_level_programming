fbcb2e9343090f3f223d6c757a52293822c764d2
alibayomy <alibayomy12@gmail.com>
2023-07-14 09:07:41 -0700

    fixing when ptr is NULL in file num 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	{
 		resize = malloc(new_size);
 		if (resize == NULL)
-			return (NULL);}
+			return (NULL);
 	}
 	if (new_size < old_size)
 	{
