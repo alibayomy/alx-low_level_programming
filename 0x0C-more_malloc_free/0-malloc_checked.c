958b65a4ec9afe58dd5909090939a69118ddbed3
alibayomy <alibayomy12@gmail.com>
2023-07-13 17:30:54 -0700

    testing file num 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdli.b>
+/**
+ * malloc_checked - llocates memory using malloc
+ * b: size of the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit (98);
+	return (p);
+}
