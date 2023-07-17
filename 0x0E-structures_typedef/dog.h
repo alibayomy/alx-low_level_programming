cea62790bb8f40f9776de33039ac138c916df6dd
alibayomy <alibayomy12@gmail.com>
2023-07-17 16:39:20 -0700

    testing the dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H_
+#define DOG_H_
+/**
+ * struct dog - new user defined type
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
