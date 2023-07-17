f7677b99c11781d2f38446809f16c8f459295949
alibayomy <alibayomy12@gmail.com>
2023-07-17 07:11:16 -0700

    pushing file num 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,8 +1,5 @@
 #ifndef _FUNCTION_LIKE_MACRO_H_
 #define _FUNCTION_LIKE_MACRO_H_
 
-#define ABS(x) (if (x > 0) \
-		(x) \
-		else \
-		(-x))
+#define ABS(x) ((x > 0) ? (x) : (-x))
 #endif /* _FUNCTION_LIKE_MACRO_H_ */
