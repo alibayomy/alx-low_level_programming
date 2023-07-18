ffe6a59298e4599560e8a331d7b2057808924c97
alibayomy <alibayomy12@gmail.com>
2023-07-18 11:30:40 -0700

    testing file num 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,8 +13,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	n_dog = malloc(sizeof(struct dog));
 	if (n_dog == NULL)
+	{
 		free(n_dog);
 		return (NULL);
+	}
 
 	n_dog->name = name;
 	n_dog->age = age;
