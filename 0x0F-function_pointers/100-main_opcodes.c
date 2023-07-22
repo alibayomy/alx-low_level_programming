835fb7106415880544186bba744e48478d3e89ef
alibayomy <alibayomy12@gmail.com>
2023-07-21 19:46:02 -0700

    could not solve

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * main - entery point
+ * @argc: number of arguments passed
+ * @argv: array of the arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv)
+{
+	int bytes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	return (0);
+}
