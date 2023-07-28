81435a01af6b3f74c68c8b37169f6a9037769598
alibayomy <alibayomy12@gmail.com>
2023-07-28 04:15:28 -0700

    test file num 100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -6,6 +6,6 @@ void my_constructor(void)__attribute__((constructor));
 
 void my_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\n
-			I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
