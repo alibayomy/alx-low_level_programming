e28b2959573f65903d326cce8fdaacb1eeaa6b7b
alibayomy <alibayomy12@gmail.com>
2023-08-01 10:06:15 -0700

    testing file num 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,8 +30,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		if (tmp == NULL)
 		{
-			free(ptr);
-			ptr = NULL;
 			return (NULL);
 		}
 		tmp = tmp->next;
