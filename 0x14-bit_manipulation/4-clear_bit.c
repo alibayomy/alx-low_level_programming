b0cf101908605e58ecc108c901b1ae169fa71ed8
alibayomy <alibayomy12@gmail.com>
2023-08-03 14:46:52 -0700

    testing file num 4

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include "0-binary_to_uint.c"
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index.
+ * @n: the number contains the bits
+ * @index: the index of the bit to be converted
+ * Return: 1 if it worked, or -1 if an error occurred
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int num = *n;
+	unsigned long int pw = power(index);
+
+	if (index > 63)
+		return (-1);
+	if (num >= pw)
+	{
+		*n = num - pw;
+		return (1);
+	}	
+	return (1);
+}
