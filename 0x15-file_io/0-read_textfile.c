20d0260bd51e3caf668935ca07d643f5663d9a4e
alibayomy <alibayomy12@gmail.com>
2023-08-07 13:23:04 -0700

    check

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
 /**
  * read_textfile - a function that reads and prints a text file
  * @filename: the name of the file to be read
@@ -9,19 +10,18 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char insidethefile[letters];
-	FILE *fp;
+	size_t fp;
 	char *buffer;
 	size_t n;
 	size_t wr;
 
-	fp = oepn(filename, O_RDONLY);
-	if (fp == NULL)
+	fp = open(filename, O_RDONLY);
+	if (fp == -1)
 		return (0);
 	buffer = malloc(sizeof(char) * letters);
 	n = read(fp, buffer, letters);
-	wr = write(STDOUT_FILENO, buf, t);
+	wr = write(STDOUT_FILENO, buffer, n);
 	free(buffer);
-	close(fd);
+	close(fp);
 	return (wr);
 }
