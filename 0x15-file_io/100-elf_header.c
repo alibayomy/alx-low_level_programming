eb53cd0924f37a51987c8996fb9818d31c3ba83c
alibayomy <alibayomy12@gmail.com>
2023-08-10 14:18:39 -0700

    test

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entery point
+ * @argc: the number of arguments passed to the main
+ * @argv: array of strings of the argument passed
+ * Return: 0 in success
+ */
+int main(int argc, char *argv[])
+{
+	int o;
+
+	o = open(argv[1], O_RDONLY);
+	if (o == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read the file%s\n", argv[1]);
+		exit(98);
+	}
+	return (0);
+}
