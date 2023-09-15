cb34c1114abe78e67227b65f8d8d768a516e7415
alibayomy <alibayomy12@gmail.com>
2023-09-14 17:50:10 -0700

    debugging

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -19,7 +19,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		after = tmp->next;
 		free(tmp);
 		*head = after;
-		*head->prev = NULL;
+		if (*head)
+			(*head)->prev = NULL;
 		return (1);
 	}
 	while (loop != index)
