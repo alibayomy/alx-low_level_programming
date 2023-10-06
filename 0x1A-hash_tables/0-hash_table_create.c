cb1933ba44565e705f04e1bef950e8942da8f328
alibayomy <alibayomy12@gmail.com>
2023-10-06 06:47:21 -0700

    testing file num 0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creating a hash table
+ * @size: the size of the array
+ * Return: a pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+
+	hash_table_t *hash_table;
+
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+	hash_table->size = size;
+	return (hash_table);
+}
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This is my answer for 0x1A-hash_tables Task month 5 ALXSE program
