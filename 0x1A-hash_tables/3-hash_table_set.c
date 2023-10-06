2d553d51f0f12d5e0df4bc2c16c09b59a6e855ff
alibayomy <alibayomy12@gmail.com>
2023-10-06 09:19:27 -0700

    adding if the value == NULL check

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,7 +12,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	char *new_value, *new_key;
 
-	if (key == NULL || ht == NULL)
+	if (key == NULL || ht == NULL || value == NULL)
 		return (0);
 	new_value = strdup(value);
 	position = key_index((const unsigned char *)key, ht->size);
