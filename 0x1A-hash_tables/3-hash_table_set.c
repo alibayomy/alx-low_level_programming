c9a0172a5a4ebfe5693c977d4fde3375e5a16455
alibayomy <alibayomy12@gmail.com>
2023-10-06 09:16:16 -0700

    testing file num 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,39 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds an element to the hash table
+ * @ht: the hash table u want to add or update
+ * @key: the key and cannot be an empty string
+ * @value: value associated with the key
+ * Return: 1 on success, else 0
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int position, i;
+	hash_node_t *new_node;
+	char *new_value, *new_key;
+
+	if (key == NULL || ht == NULL)
+		return (0);
+	new_value = strdup(value);
+	position = key_index((const unsigned char *)key, ht->size);
+	for (i = position; ht->array[i]; i++)
+	{
+		if (strcmp(ht->array[i]->key, key) == 0)
+		{
+			ht->array[i]->value = new_value;
+			return (1);
+		}
+	}
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(new_value);
+		return (0);
+	}
+	new_key = strdup(key);
+	new_node->key = new_key;
+	new_node->value = new_value;
+	new_node->next = NULL;
+	ht->array[position] = new_node;
+	return (1);
+}
