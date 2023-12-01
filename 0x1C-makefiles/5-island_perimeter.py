feefe3c3a3cf6fc15b7dd378e465b264ae5d9952
alibayomy <alibayomy12@gmail.com>
2023-12-01 12:33:59 -0800

    testing Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,100 @@
+def island_perimeter(grid): 
+    """getting the parameter of the given grid:
+        grid is a list of list of integers:
+            0 represents a water zone
+            1 represents a land zone
+            One cell is a square with side length 1
+            Grid cells are connected horizontally/vertically (not diagonally).
+            Grid is rectangular, width and height don’t exceed 100"""
+    ##getting the number of lists in the grid
+    num = 0
+    param = 0
+    for lst in grid:
+        num = num + 1
+    #i have 16 case can happen
+    i = 0
+    while (i < num):
+        for index in range (0, len (grid[i])):
+            if (grid[i][index]) == 1:
+            #check the left edge case -------
+                if (i == 0):
+                    if (index == 0):
+                        param = param + 2
+                        #check the water underneath
+                        if (grid[i][index + 1]) == 0:
+                            param += 1
+                        #check the water beside
+                        if (grid[i+1][index]) == 0:
+                            param +=1
+                    #check the right edge case
+                    elif (index == (len (grid[i]) - 1)):
+                        param = param + 2
+                        #check the water underneath
+                        if (grid[i][index - 1]) == 0:
+                            param += 1
+                        #check the water beside
+                        if (grid[i+1][index]) == 0:
+                            param +=1
+                    #check top row case
+                    else:
+                        if (grid[i][index +1]) == 0 :
+                            param +=1
+                        if (grid[i][index -1]) == 0 :
+                            param +=1
+                        if (grid[i+1][index]) == 0 :
+                            param +=1
+                #check the bottom edge cases
+                elif (i == num - 1):
+                    if (index == 0):
+                        param = param + 2
+                        #check the water underneath
+                        if (grid[i][index + 1]) == 0:
+                            param += 1
+                        #check the water beside
+                        if (grid[i-1][index]) == 0:
+                            param +=1
+                    #check the right edge case
+                    elif (index == (len (grid[i]) - 1)):
+                        param = param + 2
+                        #check the water underneath
+                        if (grid[i][index - 1]) == 0:
+                            param += 1
+                        #check the water beside
+                        if (grid[i-1][index]) == 0:
+                            param +=1
+                    #check bottom row case
+                    else:
+                        if (grid[i][index +1]) == 0 :
+                            param +=1
+                        if (grid[i][index -1]) == 0 :
+                            param +=1
+                        if (grid[i-1][index]) == 0 :
+                            param +=1
+                else:
+                    ## check the mid  edge cases
+                    if(index == 0 ):
+                        param +=1
+                        if(grid[i][index + 1] == 0):
+                            param +=1
+                        if(grid[i-1][index] == 0):
+                            param +=1
+                        if(grid[i+1][index] == 0):
+                            param +=1
+                    elif(index == num - 1):
+                        if(grid[i][index - 1] == 0):
+                            param +=1
+                        if(grid[i-1][index] == 0):
+                            param +=1
+                        if(grid[i+1][index] == 0):
+                            param +=1
+                    else:
+                        if(grid[i][index + 1] == 0):
+                            param +=1
+                        if(grid[i-1][index] == 0):
+                            param +=1
+                        if(grid[i+1][index] == 0):
+                            param +=1
+                        if(grid[i][index-1] == 0):
+                            param += 1
+        i+=1
+    return param    
